b399bb9c3f76578e45024ee7fe56ff67695d5bf2
Carlos Hernández <carloshinzunza2@gmail.com>
2026-03-16 11:33:35 -0600

    input adornment

--- src/features/trainee-table/trainee-table.component.tsx
+++ src/features/trainee-table/trainee-table.component.tsx
@@ -5,6 +5,8 @@ import { useTrainee } from "@/hooks/useTrainee";
 import { GenericTable } from "@/shared/ui/GenericTable.component";
 import { getTraineeColumns } from "./trainee-columns";
 import type { traineeType } from "@/models/trainee/trainee.type";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 
 export default function TraineeTable() {
   const { data, pagination, pageCount, status, handlePaginationChange } = useTrainee();
@@ -26,6 +28,16 @@ export default function TraineeTable() {
 
   return (
     <div className="w-full">
+     <div className="flex items-center justify-end">
+  <div className="relative w-full max-w-xs my-2">
+    <Search className="pointer-events-none absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+    <Input
+      type="text"
+      placeholder="Buscar alumno"
+      className="pl-9" // add left padding so text starts after the icon
+    />
+  </div>
+</div>
   <GenericTable
       maxBodyHeight="calc(100vh - 250px)"
       data={data}
